557abba89f26bef9e271275103118cc3f8c385bd
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-03-30 15:24:44 +0300

    Update ClassChecker.java

--- src/by/epam/javatraining/krupin/tasks/maintask2/model/logic/ClassChecker.java
+++ src/by/epam/javatraining/krupin/tasks/maintask2/model/logic/ClassChecker.java
@@ -1,6 +1,9 @@
 package by.epam.javatraining.krupin.tasks.maintask2.model.logic;
 
 
+import by.epam.javatraining.krupin.tasks.maintask2.model.data.Taxopark;
+import by.epam.javatraining.krupin.tasks.maintask2.model.data.Transport;
+
 public class ClassChecker {
 
     public static void checkPositiveNumber(double... mas) throws Exception {
@@ -8,6 +11,12 @@ public class ClassChecker {
             if (mas[i] < 0) {
                 throw new Exception("Wrong variable");
             }
+    }
+
+    public static void checkTaxoparkIndex(Transport[] transports, int index) throws Exception {
+        if(index < 0 || index > transports.length){
+            throw new Exception("Wrong index!");
+        }
     }
 
 }
